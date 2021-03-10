fddcec15ca614de9e5c88660524929fb584a110a
Marco.seb <marco.seba96@gmail.com>
2021-03-10 22:41:29 +0100

    upgrade add partials php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Sezione home della Molisana')
+
+
+@section('content')
+    
+@endsection
---
+++ resources/views/news.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Sezione Notizie della Molisana')
+
+@section('content')
+    
+@endsection
