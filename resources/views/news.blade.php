1c67b9b65fea5ac72b3c389a1122b1f2363feb90
Marco.seb <marco.seba96@gmail.com>
2021-03-11 01:40:26 +0100

    upgrade page news

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,5 +3,9 @@
 @section('title', 'Sezione Notizie della Molisana')
 
 @section('content')
-    
+    <div class="container">
+        <div class="box-news">
+            
+        </div>
+    </div>
 @endsection
