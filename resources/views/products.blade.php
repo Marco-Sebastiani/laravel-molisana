acac062bd2d260b25a345914e4ccf615341f6761
Marco.seb <marco.seba96@gmail.com>
2021-03-11 01:27:59 +0100

    upgrade page products

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,5 +4,18 @@
 
 
 @section('content')
-    
+    <div class="container">
+        <div class="box-cards">
+            @foreach ($formati as $formato)
+                <div class="box-item">
+                    <img src="{{$formato['src']}}" alt="{{$formato['titolo']}}">
+                    <div class="box-layer">
+                        <a href="">{{$formato['titolo']}} - {{$formato['tipo']}}</a>
+                    </div>
+                </div>
+
+            @endforeach
+
+        </div>
+    </div>
 @endsection
